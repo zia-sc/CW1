1aaaba74fd7b2965b71f2d0d90f05dccfc8228af
zia-sc <ziasherchan25@gmail.com>
2024-11-28 15:39:42 +0000

    files added to my respo

---
+++ lessons.js
@@ -0,0 +1,89 @@
+let lessons=[
+    {
+        "title": "Mathematics",
+        "description" :"Learn basic arithmetic, algebra, and problem-solving techniques in a fun and interactive way.",
+        "price":65,
+        "image":"images/accountclass.jpeg",
+        "availableInventory":3,
+        "rating":2
+        
+        
+    },
+    {
+        "title":"Science",
+        "description":"Discover the wonders of science through engaging experiments.",
+        "price":75,
+        "image":"images/scienceclass.jpeg",
+        "availableInventory":3,
+        "rating":4
+
+
+    },
+    {
+        "title":"Art",
+        "description":"Unleash your creativity with this introductory art class",
+        "price":40,
+        "image": "images/artlessons.jpeg",
+        "availableInventory":3,
+        "rating":5
+    },
+    {
+        "title":"Creative Writing",
+        "description":"Unleash your inner storyteller! ",
+        "price":45,
+        "image": "images/creativewritingclass.jpeg",
+        "availableInventory":3,
+        "rating":3
+    },
+    {
+        "title":"History",
+        "description":"Dive into the rich tapestry of global history!",
+        "price":55,
+        "image": "images/historyclass.jpeg",
+        "availableInventory":3,
+        "rating":4
+    },
+    {
+        "title":"Economy",
+        "description":"Learn the fundamentals of economics, including supply and demand, market structures, and the role of government in the economy.",
+        "price":45,
+        "image": "images/economyclass.jpeg",
+        "availableInventory":3,
+        "rating":5
+    },
+    {
+        "title":"Account",
+        "description":"A great start for future accountants or anyone wanting to manage their finances better.",
+        "price":65,
+        "image": "images/accountclass.jpeg",
+        "availableInventory":3,
+        "rating":3
+    },
+    {
+        "title":"Public Speaking and Confidence",
+        "description":"Public Speaking and Confidence",
+        "price":40,
+        "image": "images/publicspeakingclass.png",
+        "availableInventory":3,
+        "rating":5
+    },
+    {
+        "title":"Computer Science",
+        "description":"Get a head start in the world of technology by learning the basics of coding. ",
+        "price":55,
+        "image": "images/computerlessons.jpeg",
+        "availableInventory":3,
+        "rating":4
+    },
+    {
+        "title":"Music Lessons (Guitar)",
+        "description":"Learn to play the guitar in this beginner's class.",
+        "price":45,
+        "image": "images/musiclessons.jpeg",
+        "availableInventory":3,
+        "rating":3
+    },
+
+
+
+]
